cdbbe26671f0d14bf75efd2baf52f7a4a35385a8
OsheaReid <oreid10@my.centennialcollege.ca>
2024-03-25 21:41:18 -0400

    first commit

---
+++ src/main/java/com/lab2/myweb/Myweblab2Application.java
@@ -0,0 +1,13 @@
+package com.lab2.myweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myweblab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myweblab2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab2/myweb/Myweblab2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab2.myweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myweblab2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
